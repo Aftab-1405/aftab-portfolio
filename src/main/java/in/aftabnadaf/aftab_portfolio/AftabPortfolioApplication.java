e47e0cc26f83de9bd4681054506d53b1c9db5932
Aftab Nadaf <nadafaftab97@gmail.com>
2025-05-31 19:08:21 +0530

    This is my project structure hierarchy.

---
+++ src/main/java/in/aftabnadaf/aftab_portfolio/AftabPortfolioApplication.java
@@ -0,0 +1,13 @@
+package in.aftabnadaf.aftab_portfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AftabPortfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AftabPortfolioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/in/aftabnadaf/aftab_portfolio/AftabPortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package in.aftabnadaf.aftab_portfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AftabPortfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
